033221aa88d0ba939f9731e7c403ec0bf998a726
Jeeshan Sheikh <jeeshany2k26@gmail.com>
2026-02-14 18:01:39 +0530

    Add files via upload

---
+++ notes.md
@@ -0,0 +1,28 @@
+### Understanding t-swap protocol
+
+pool factory contract helps us create pools of tokens (each pool is a pair of `weth X erc20 token`) 
+
+the t-swap-pool contract will facilitate the exchange between the weth and erc20 tokens and will have the logic to balance the pools
+
+
+### Testing
+
+Fuzzing/Fuzz test ---> stateless fuzz testing
+
+Here we give random input to a particular function
+
+Invariance -----> statefull fuzz testing
+
+here we give random data to random functions
+
+FINDING LAYOUT 
+
+### [S-#] TITLE (Root Cause + Impact)
+
+**Description:** 
+
+**Impact:** 
+
+**Proof of Concept:**
+
+**Recommended Mitigation:** 
---
+++ src/PoolFactory.sol
@@ -0,0 +1,78 @@
+/**
+ * /-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\|/-\
+ * |                                     |
+ * \ _____    ____                       /
+ * -|_   _|  / ___|_      ____ _ _ __    -
+ * /  | |____\___ \ \ /\ / / _` | '_ \   \
+ * |  | |_____|__) \ V  V / (_| | |_) |  |
+ * \  |_|    |____/ \_/\_/ \__,_| .__/   /
+ * -                            |_|      -
+ * / hello                                    \
+ * |                                     |
+ * \-/|\-/|\-/|\-/|\-/|\-/|\-/|\-/|\-/|\-/
+ */
+// SPDX-License-Identifier: GNU General Public License v3.0
+pragma solidity 0.8.20;
+
+import { TSwapPool } from "./TSwapPool.sol";
+import { IERC20 } from "forge-std/interfaces/IERC20.sol";
+
+contract PoolFactory {
+    error PoolFactory__PoolAlreadyExists(address tokenAddress);
+    error PoolFactory__PoolDoesNotExist(address tokenAddress);
+    // @audit info this error isn't used anywhere
+
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+    mapping(address token => address pool) private s_pools;
+    mapping(address pool => address token) private s_tokens;
+
+    address private immutable i_wethToken;
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+    event PoolCreated(address tokenAddress, address poolAddress);
+
+    /*//////////////////////////////////////////////////////////////
+                               FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    constructor(address wethToken) {
+        i_wethToken = wethToken;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function createPool(address tokenAddress) external returns (address) {
+        if (s_pools[tokenAddress] != address(0)) {
+            revert PoolFactory__PoolAlreadyExists(tokenAddress);
+        }
+        //q what if the name function of the ERC20 reverts
+        //always be suspiscious around ERC20s
+        string memory liquidityTokenName = string.concat("T-Swap ", IERC20(tokenAddress).name());
+        string memory liquidityTokenSymbol = string.concat("ts", IERC20(tokenAddress).name()); //@audit .symbol should be used
+        TSwapPool tPool = new TSwapPool(tokenAddress, i_wethToken, liquidityTokenName, liquidityTokenSymbol);
+        //deploying of a new pool (tokenaddress X i_wethtoken)
+        s_pools[tokenAddress] = address(tPool);
+        s_tokens[address(tPool)] = tokenAddress;
+        emit PoolCreated(tokenAddress, address(tPool));
+        return address(tPool);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                   EXTERNAL AND PUBLIC VIEW AND PURE
+    //////////////////////////////////////////////////////////////*/
+    function getPool(address tokenAddress) external view returns (address) {
+        return s_pools[tokenAddress];
+    }
+
+    function getToken(address pool) external view returns (address) {
+        return s_tokens[pool];
+    }
+
+    function getWethToken() external view returns (address) {
+        return i_wethToken;
+    }
+}
